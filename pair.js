b079b72a05d0faa04e588714711d826cb07a856a
uke224 <mathishajovindu@gmail.com>
2025-07-26 15:59:44 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -79,9 +79,9 @@ router.get("/", async (req, res) => {
             const string_session = mega_url.replace(
               "https://mega.nz/file/",
               ""
-            );
+            );https://ibb.co/N6WpqWp0
 
-            const sid = `*ROBIN [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
+            const sid = `*cyber xmd [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
